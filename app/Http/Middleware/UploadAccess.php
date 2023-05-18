5a003c316a39b33cc766cdf4b692f94fd1c3db77
Axel <axel@webinno.fr>
2023-05-18 10:50:58 +0200

    Improve authentication check

--- app/Http/Middleware/UploadAccess.php
+++ app/Http/Middleware/UploadAccess.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Helpers\Upload;
+use Illuminate\Support\Facades\Storage;
 
 class UploadAccess
 {
@@ -18,16 +19,25 @@ class UploadAccess
  	*/
 	public function handle(Request $request, Closure $next): Response
 	{
-		if ($request->session()->missing('authenticated') && Upload::canUpload($request->ip()) !== true) {
-			//return redirect('login');
-			if ($request->ajax()) {
-				abort(401);
-			}
-			else {
-				return response()->view('login');
+		// Checking IP based access
+		if (Upload::canUpload($request->ip()) === true) {
+			return $next($request);
+		}
+
+		// Checking credentials auth
+		if ($request->session()->get('authenticated', false) === true && $request->session()->has('login')) {
+			// If user still exists
+			if (Storage::disk('users')->exists($request->session()->get('login').'.json')) {
+				return $next($request);
 			}
 		}
 
-		return $next($request);
+		// Fallback, authentication required
+		if ($request->ajax()) {
+			abort(401);
+		}
+		else {
+			return response()->view('login');
+		}
 	}
 }
