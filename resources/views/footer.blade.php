7ac0f95312058c5fb9aa57ecb5682bbce7a99701
escix <preminik@preminik.com>
2023-11-16 23:59:40 +1100

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -11,11 +11,6 @@
 	@endif
 
 	<div class="absolute right-0 top-0 text-[.6rem] text-slate-100 text-right px-2 py-1 italic bg-primary rounded-tl-lg">
-		Made with
-		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-primary-light fill-primary-superlight inline w-4 h-4">
-	  	<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-		</svg>
-	 	by
-		<a class="text-white" href="https://github.com/axeloz" target="_blank">axeloz</a>
+		By <a class="text-white" href="https://strac.com.au" target="_blank">Strac Consulting Engineers</a>
 	</div>
 </footer>
