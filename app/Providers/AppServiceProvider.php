c9698c3dd200c2059a93988dee9bfae2a02f2fd1
escix <preminik@preminik.com>
2023-11-18 10:48:09 +1100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+		if($this->app->environment('production')) {
+    	\URL::forceScheme('https');
+}
     }
 }
