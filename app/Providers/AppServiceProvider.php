2fdca6b0c730c0ea8f0f6ebe5d8e2b55a7392e02
Axel <axel@webinno.fr>
2024-01-06 14:28:53 +0100

    Fixing support of HTTP_X_FORWARDED_PROTO from proxy on top of Docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+		if (! empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+			\URL::forceScheme('https');
+		}
     }
 }
